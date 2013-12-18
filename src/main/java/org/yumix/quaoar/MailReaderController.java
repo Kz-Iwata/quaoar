61901d07cc0481a33eb97bd0a61501539060f4d4
Yumi Hiraoka <yumix@outlook.com>
2013-12-19 07:14:30 +0900

    Create all views

--- src/main/java/org/yumix/quaoar/MailReaderController.java
+++ src/main/java/org/yumix/quaoar/MailReaderController.java
@@ -1,5 +1,15 @@
 package org.yumix.quaoar;
 
-public class MailReaderController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
+public class MailReaderController {
+	@FXML
+	private Button back;
+	
+	@FXML
+	public void onBackAction(ActionEvent event) {
+		
+	}
 }
--- src/main/java/org/yumix/quaoar/MailWriterController.java
+++ src/main/java/org/yumix/quaoar/MailWriterController.java
@@ -1,5 +1,23 @@
 package org.yumix.quaoar;
 
-public class MailWriterController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
+public class MailWriterController {
+	@FXML
+	private Button send;
+	
+	@FXML
+	private Button cancel;
+	
+	@FXML
+	public void onClickSend(ActionEvent event) {
+		
+	}
+	
+	@FXML
+	public void onClickCancel(ActionEvent event) {
+		
+	}
 }
