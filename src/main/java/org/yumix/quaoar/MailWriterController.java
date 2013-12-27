e286dd121b7e356879e5a17a45022eb7871ef874
Yumi Hiraoka <yumix@outlook.com>
2013-12-28 04:55:30 +0900

    メール作成画面をとりあえず実装（結構穴だらけ）

--- src/main/java/org/yumix/quaoar/MailWriterController.java
+++ src/main/java/org/yumix/quaoar/MailWriterController.java
@@ -1,12 +1,24 @@
 package org.yumix.quaoar;
 
-import javax.inject.Inject;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-public class MailWriterController {
+import javax.inject.Inject;
+import javax.mail.Session;
+
+import org.yumix.mail.sender.Mail;
+import org.yumix.mail.session.UserSetting;
+import org.yumix.mail.session.SessionProvider;
+import org.yumix.mail.session.qualifier.ICloud;
+
+public class MailWriterController implements Initializable {
 	
 	@Inject
 	private QuaoarRunner quaoarRunner;
@@ -18,12 +30,52 @@ public class MailWriterController {
 	private Button cancel;
 	
 	@FXML
+	private TextField fromAddr;
+	
+	@FXML
+	private TextField ccAddr;
+	
+	@FXML
+	private TextField bccAddr;
+	
+	@FXML
+	private TextField toAddr;
+	
+	@FXML
+	private TextField subject;
+	
+	@FXML
+	private TextArea messageBody;
+	
+	@Inject
+	private UserSetting userSetting;
+	
+	@Inject
+	@ICloud
+	private SessionProvider sessionProvider;
+	
+	@FXML
 	public void onClickSend(ActionEvent event) {
+		String[] toList = this.toAddr.getText().split("[,;]");
+		String[] ccList = this.ccAddr.getText().split("[,;]");
+		String[] bccList = this.bccAddr.getText().split("[,;]");
+		String subject = this.subject.getText();
+		String message = this.messageBody.getText();
 		
+//		Session session = SessionProviderFactory.getInstance().getSession(ICloudProvider.class, from, "yumix_h@icloud.com", "Dysnomi@0");
+		Session session = sessionProvider.getSession();
+		Mail.from(userSetting.getFromAddress()).to(toList).cc(ccList).bcc(bccList).subject(subject).message(message).send(session);
+
+		quaoarRunner.select(WhichScene.MAILBOX);
 	}
 	
 	@FXML
 	public void onClickCancel(ActionEvent event) {
 		quaoarRunner.select(WhichScene.MAILBOX);
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		fromAddr.setText(userSetting.getFromAddress());
 	}
 }
